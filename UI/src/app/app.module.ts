a9d63fe40214dce0ba73a5e63ddfbc995d8b1589
Colin MacMaster <colinmacmaster@Colins-MacBook-Air.local>
2023-09-22 20:47:59 -0400

    upgraded angular and added material

--- UI/src/app/app.module.ts
+++ UI/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http'
 import { FormsModule } from '@angular/forms';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [
@@ -14,6 +15,7 @@ import { FormsModule } from '@angular/forms';
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
+    MatButtonModule,
     FormsModule
   ],
   providers: [],
